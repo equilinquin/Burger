5cbe5128fd4f04a77e4eb0fee09b1e3213d6f4fc
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-05 00:01:56 -0800

    adjusted code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,10 +22,7 @@ router.put("/api/burgers/:id", function(req, res) {
   const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
-  console.log(req.body);
-  burgers.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
+  burgers.update(req.body.devoured, condition, function(result) {
     if (result.changedRows == 0) {
       res.status(404).end();
     } else {
