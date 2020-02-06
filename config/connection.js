c85013e72026d7750390f7396f49323b9969f45a
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-05 20:30:06 -0800

    resolved connection issue

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql");
+var connection;
 
 if(process.env.JAWSDB_URL) {
-  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
