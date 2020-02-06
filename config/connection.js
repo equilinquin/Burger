dca0e0c108e25770da1b5074a58bb75cd4933c3a
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-05 20:06:37 -0800

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_uURL) {
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "liz2007",
   database: "burgers_db"
 });
+};
+
 
 connection.connect(function(err) {
   if (err) {
